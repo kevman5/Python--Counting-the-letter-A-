3d27d43170f2b347ba2d7546768158ce9da3b719
KevCodes <41352514+kevman5@users.noreply.github.com>
2021-12-18 16:29:01 -0600

    Add files via upload

---
+++ counting a's.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 20:14:44 2021
+
+@author: kevmm
+"""
+
+word1 = input("Enter banana: ")
+index = 0
+count = 0
+for letter in word1:
+    if letter == 'a':
+        count = count + 1
+        print("The number of a's in banana =",count)
+        
+       
+        
+            
